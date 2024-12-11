38cc6696fd0aed51114d0a8cbe69e1fd7c404b9a
Elias Kaiser <elias@mbp-von-elias.speedport.ip>
2024-12-11 13:00:08 +0100

    Added debugging functionalities + added path_constraints

--- displib_solution.py
+++ displib_solution.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import gurobipy as gp
 import networkx as nx
+import matplotlib.pyplot as plt
 
 
 class Instance:
@@ -16,28 +17,62 @@ class DisplibSolver:
         self.objectives = instance.objectives
         self.graphs = [create_graph(train) for train in self.trains]
 
+        if args.debug:
+            save_graphs_as_image(self.graphs)
+
         self.model = gp.Model()
-        self.start_vars = []
-        self.select_vars = []
+        self.op_start_vars = []
+        self.edge_select_vars = []
 
         for i, train in enumerate(self.trains):
-            op_start_vars = []
-            op_select_vars = []
+            start_vars = []
+            select_vars = {}
 
             # inline declaration possible if ub is infinite per default
             for j, op in enumerate(train):
                 if op["start_ub"]:
-                    op_start_vars.append(self.model.addVar(vtype=gp.GRB.INTEGER, lb=op["start_lb"], ub=op["start_ub"],
-                                                           name=f"Start Train {i} : Operation {j}"))
+                    start_vars.append(self.model.addVar(vtype=gp.GRB.INTEGER, lb=op["start_lb"], ub=op["start_ub"],
+                                                        name=f"Train {i} : Operation {j}"))
                 else:
-                    op_start_vars.append(self.model.addVar(vtype=gp.GRB.INTEGER, lb=op["start_lb"]))
-                op_select_vars.append(self.model.addVar(vtype=gp.GRB.BINARY, name=f"Select Train {i} : Operation {j}"))
-            self.start_vars.append(op_start_vars)
-            self.select_vars.append(op_select_vars)
+                    start_vars.append(self.model.addVar(vtype=gp.GRB.INTEGER, lb=op["start_lb"],
+                                                        name=f"Train {i} : Operation {j}"))
 
+                for s in op["successors"]:
+                    select_vars[(j, s)] = self.model.addVar(vtype=gp.GRB.BINARY,
+                                                            name=f"Train {i} : Edge<{j},{s}>")
+            self.op_start_vars.append(start_vars)
+            self.edge_select_vars.append(select_vars)
+
+        self.add_path_constrains()
+        self.add_timing_constraints()
+
+
+    def add_path_constrains(self):
+        for i, train in enumerate(self.trains):
+            # exactly one out_edge for first operation, one in_edge for last operation
+            self.model.addConstr(gp.quicksum(self.edge_select_vars[i][out_edge]
+                                             for out_edge in self.graphs[i].out_edges(0)) == 1)
+
+            self.model.addConstr(gp.quicksum(self.edge_select_vars[i][in_edge]
+                                             for in_edge in self.graphs[i].in_edges(len(self.graphs[i].nodes) - 1)) == 1)
+
+            # If a vertex is not the first nor the last one, make sure the number of in egdes is equal to the number of out edges
+            # Not necessary to make sure there's max. 1 in_/out_edges selected, because first and last op already have only 1 edge selected
+            for j in range(1, len(self.graphs[i].nodes) - 1):
+                ins = self.graphs[i].in_edges(j)
+                outs = self.graphs[i].out_edges(j)
+                self.model.addConstr(gp.quicksum(self.edge_select_vars[i][in_edge] for in_edge in ins) ==
+                                     gp.quicksum(self.edge_select_vars[i][out_edge] for out_edge in outs))
+
+    def add_timing_constraints(self):
+        return
 
     def solve(self):
         self.model.optimize()
+
+        if self.model.status == gp.GRB.OPTIMAL:
+            for v in self.model.getVars():
+                print(f"{v.VarName} = {v.X}")
 
         # Just to check for now
         events = [  {"time": 0, "train": 0, "operation": 0},
@@ -80,6 +115,7 @@ def main():
 def create_graph(train):
     graph = nx.DiGraph()
     graph.add_nodes_from([i for i, _ in enumerate(train)])
+
     for i, operation in enumerate(train):
         graph.add_edges_from([(i, v) for v in operation["successors"]])
     return graph
@@ -116,9 +152,48 @@ def parse_instance(instance):
     return Instance(instance["trains"], instance["objective"])
 
 
+def save_graphs_as_image(graphs):
+    print("Creating graphs...")
+
+    for i, graph in enumerate(graphs):
+        depth = nx.single_source_shortest_path_length(graph, 0)
+
+        depth_groups = {}
+        for node, d in depth.items():
+            if d not in depth_groups:
+                depth_groups[d] = []
+            depth_groups[d].append(node)
+
+        pos = {}
+        for d, nodes in depth_groups.items():
+            count = len(nodes)
+            for j, node in enumerate(sorted(nodes)):
+                pos[node] = (d, -((count - 1) / 2) + j)
+
+        x = nx.shortest_path_length(graph, 0, len(graph.nodes) - 1)
+        plt.figure(figsize=(x, 5))
+        nx.draw(
+            graph,
+            pos,
+            with_labels=True,
+            node_size=700,
+            node_color="lightblue",
+            font_size=10,
+            font_color="black",
+            edge_color="gray",
+            arrows=True,
+            arrowsize=20
+        )
+
+        plt.savefig(f"Graphs/graph{i}.png", format="png")
+        plt.close()
+    print("Done!")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="DISPLIB-Solver", description="By Lina Breuer, Sebastian Brunke, Elias Kaiser, Felix Michel")
     parser.add_argument('instance', help="Filename of the instance that needs to be solved", type=str)
+    parser.add_argument("--debug", action="store_true", help="Activates debug-mode")
     args = parser.parse_args()
 
     main()

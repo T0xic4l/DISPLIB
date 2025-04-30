5601734d6ad811f9e9161312f18ba50c9f8fab94
Elias Kaiser <elias.kaiser@tu-braunschweig.de>
2025-04-30 20:35:12 +0200

    Extracted instances that we want to solve a few more times

--- verifyscript.py
+++ verifyscript.py
@@ -3,4 +3,5 @@ import subprocess
 
 
 for instance in [f for f in os.listdir("Instances") if f.endswith(".json")]:
-    subprocess.run(f"python displib_verify.py Instances/{instance} Solutions/sol_{instance}")
+    print(instance)
+    subprocess.run(f"python displib_verify.py Instances/{instance} Solutions/10min_sol_{instance}")
